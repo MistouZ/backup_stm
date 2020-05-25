d6eea80f92b719dc80618497be7765cff3a5d4fc
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-26 09:23:05 +1100

    Backup Contact

--- backup_contact.php
+++ backup_contact.php
@@ -18,6 +18,9 @@ $bd = new PDO('mysql:host=localhost;port=3306; dbname=stm_test_db;charset=utf8',
 
 $count = $bd->query('SELECT * FROM dossier WHERE societe="concept" OR societe ="concerto" OR societe="agence" OR societe = "itech" AND annee >= 2018 GROUP BY client');
 $maxrow = $count->rowCount();
+
+echo $maxrow;
+
 /*
 $req = $bd->query('SELECT * FROM dossier WHERE societe="concept" OR societe ="concerto" OR societe="agence" OR societe = "itech"  AND annee >= 2018 GROUP BY client LIMIT '.$i.',1 ');
 $recup = $req->fetch();
