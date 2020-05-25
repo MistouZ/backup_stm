e085a604da2463e1c84d3755a6e07baa74f028f9
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-25 14:15:26 +1100

    Create customer

--- _pages/_post/creer_client.php
+++ _pages/_post/creer_client.php
@@ -28,7 +28,7 @@ $customer = new Customers($array);
 $customermanager = new CustomersManager($bdd);
 
 print_r($customer);
-
+/*
 $test = $customermanager->add($customer, $_POST["case"], $_POST["account"],$_POST["subaccount"], $_POST["taxes"]);
 
 
