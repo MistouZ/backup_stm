d98ffc9db080d1b00e100c187f0e29be1b0420f6
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-25 11:53:34 +1100

    Remove comment

--- backup_customers.php
+++ backup_customers.php
@@ -27,8 +27,9 @@ $maxrow = $count->rowCount();
 $req = $bd->query('SELECT * FROM dossier WHERE societe="concept" OR societe ="concerto" OR societe="agence" OR societe = "itech"  AND annee > 2018 LIMIT '.$i.',1 ');
 $recup = $req->fetch();
 
-print_r($recup);
-
+//print_r($recup);
+$request = 'SELECT * FROM client WHERE nom="'.$recup["client"].'" LIMIT '.$i.',1';
+print_r($request);
 $reponse = $bd->query('SELECT * FROM client WHERE nom="'.$recup["client"].'" LIMIT '.$i.',1');
 $donnees = $reponse->fetch();
 
