b8fcda261cf9cee4c234a928231a17414bfa144c
Amaury DEWYNTER <adewynter@concept.nc>
2020-05-25 12:09:42 +1100

    Class change

--- _cfg/classes/class_db.php
+++ _cfg/classes/class_db.php
@@ -8,11 +8,10 @@
 
 class DB
 {
-    private $host = 'mysql:host=localhost;port=3306; dbname=exotic_stm';
-    private $username = 'exotic-adm';
-    private $password = 'N*v40i4p';
+    private $host = 'mysql:host=localhost;port=3306; dbname=stm_test_db';
+    private $username = 'testuser';
+    private $password = 'U!nx837j';
     private $bdd;
-
 
     /**
      * DB constructor.
