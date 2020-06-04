2ca0811fe66f4ca44b4aa1aea1e94050c4765752
Amaury DEWYNTER <adewynter@concept.nc>
2020-06-04 13:59:08 +1100

    backup supplier

--- backup_suppliers.php
+++ backup_suppliers.php
@@ -15,7 +15,7 @@ else
 {
     $row=0;
 }
-
+/*
 //récupération des données de l'utilisateur
 $bd = new PDO('mysql:host=localhost;port=3306; dbname=stm_test_db;charset=utf8', 'testuser', 'U!nx837j');
 
