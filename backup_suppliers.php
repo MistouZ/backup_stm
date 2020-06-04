aec23ffdf47b5e7e3fa6ff8676bf9bed9b18cc07
Amaury DEWYNTER <adewynter@concept.nc>
2020-06-04 11:56:58 +1100

    backup supplier

--- backup_suppliers.php
+++ backup_suppliers.php
@@ -18,10 +18,10 @@ else
 
 //récupération des données de l'utilisateur
 $bd = new PDO('mysql:host=localhost;port=3306; dbname=stm_test_db;charset=utf8', 'testuser', 'U!nx837j');
-/*
+
 $count = $bd->query('SELECT * FROM dossier  WHERE societe !="nmcp" AND societe !="hydro" GROUP BY client');
 $maxrow = $count->rowCount();
-
+/*
 $req = $bd->query('SELECT * FROM dossier  WHERE societe !="nmcp" AND societe !="hydro" GROUP BY client LIMIT '.$i.',1 ');
 $recup = $req->fetch();
 
