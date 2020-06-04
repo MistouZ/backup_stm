4a3dd4c2b66c013883b312ef37da365f4223cfad
Amaury DEWYNTER <adewynter@concept.nc>
2020-06-04 11:53:10 +1100

    backup supplier

--- _pages/_post/creer_fournisseur.php
+++ _pages/_post/creer_fournisseur.php
@@ -7,34 +7,47 @@
  */
 include("../../_cfg/cfg.php");
 
-if(isset($_POST['valider'])) {
-    $name = $_POST['name'];
-    $physical_address = $_POST['physical_address'];
-    if ($_POST["invoice_address"] == NULL) {
-        $invoice_address = $_POST['physical_address'];
-    } else {
-        $invoice_address = $_POST['invoice_address'];
-    }
+$name=$_POST['name'];
+$physical_address=$_POST['physical_address'];
 
-    $is_active = 1;
-
-    $array = array(
-        'name' => $name,
-        'physicalAddress' => $physical_address,
-        'invoiceAddress' => $invoice_address,
-        'isActive' => $is_active
-    );
-
-    $supplier = new Suppliers($array);
-    $suppliermanager = new SuppliersManager($bdd);
-    $test = $suppliermanager->add($supplier, $_POST["case"],$_POST["account"],$_POST["subaccount"]);
-
-    if (is_null($test)) {
-        header('Location: ' . URLHOST . $_COOKIE['company'] . "/fournisseur/afficher/error");
-    } else {
-        header('Location: ' . URLHOST . $_COOKIE['company'] . "/fournisseur/afficher/success");
-    }
-
-
+if($_POST["invoice_address"] == NULL)
+{
+    $invoice_address=$_POST['physical_address'];
 }
+else{
+    $invoice_address=$_POST['invoice_address'];
+}
+
+$is_active =1;
+
+$array = array(
+    'name' => $name,
+    'physicalAddress' => $physical_address,
+    'invoiceAddress' => $invoice_address,
+    'isActive' => $is_active
+);
+
+$row = $_POST["row"];
+$maxrow = $_POST["maxrow"];
+
+$customer = new Customers($array);
+$customermanager = new CustomersManager($bdd);
+
+
+$test = $suppliermanager->add($supplier, $_POST["case"],$_POST["account"],$_POST["subaccount"]);
+
+print_r($test);
+
+if(!is_null($test))
+{
+    if($row < $maxrow) {
+        $row++;
+        //on ajoute 1 à la limite pour lire la prochaine ligne
+        header('Location: http://test.bitwin.nc/backup_suppliers.php?row='.$row);
+    }
+    else{
+        header('Location: http://test.bitwin.nc/index.php');
+    }
+}
+
 ?>
