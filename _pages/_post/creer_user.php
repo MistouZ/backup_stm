253ac90a4c91f5738fdc4f2e1bac50a0c23378f7
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-08-07 11:38:37 +1100

    user creation

--- _pages/_post/creer_user.php
+++ _pages/_post/creer_user.php
@@ -44,8 +44,8 @@ if(isset($_POST['valider'])) {
 
 }
 if(is_null($test)){
-    header('Location: '.URLHOST.$_COOKIE['company']."/user/afficher/error");
+    header('Location: '.URLHOST.$_COOKIE['company']."user/afficher/error");
 }else{
-    header('Location: '.URLHOST.$_COOKIE['company']."/user/afficher/success");
+    header('Location: '.URLHOST.$_COOKIE['company']."user/afficher/success");
 }
 ?>
