5b3aad85356d310547e316ca83895531f58e7666
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-08-07 11:51:39 +1100

    creer client

--- _pages/_post/creer_client.php
+++ _pages/_post/creer_client.php
@@ -1,50 +1,46 @@
 <?php
 include("../../_cfg/cfg.php");
-    
+$name=$_POST['name'];
+$physical_address=$_POST['physical_address'];
+if($_POST["invoice_address"] == NULL)
+{
+    $invoice_address=$_POST['physical_address'];
+}
+else{
+    $invoice_address=$_POST['invoice_address'];
+}
+$is_active =1;
+$array = array(
+    'name' => $name,
+    'physicalAddress' => $physical_address,
+    'invoiceAddress' => $invoice_address,
+    'isActive' => $is_active
+);
+$row = $_POST["row"];
+$maxrow = $_POST["maxrow"];
+$customer = new Customers($array);
+$customermanager = new CustomersManager($bdd);
 
-if(isset($_POST['valider'])){
-		$name=$_POST['name'];
-		$physical_address=$_POST['physical_address'];
-    if($_POST["invoice_address"] == NULL)
-    {
-        $invoice_address=$_POST['physical_address'];
+echo $maxrow;
+
+$test = $customermanager->add($customer, $_POST["case"], $_POST["account"],$_POST["subaccount"], $_POST["taxes"]);
+
+print_r($test);
+if(!is_null($test))
+{
+    if($row < $maxrow) {
+        $row++;
+        //on ajoute 1 à la limite pour lire la prochaine ligne
+        header('Location: http://test.bitwin.nc/backup_customers.php?row='.$row);
     }
     else{
-      $invoice_address=$_POST['invoice_address'];
-    }
-    if(isset($_POST["is_supplier"]))
-    {
-      $supplier = 1;
-    }
-    else{
-      $supplier = 0;
-    }
-
-    $is_active =1;
-
-    $array = array(
-        'name' => $name,
-        'physicalAddress' => $physical_address,
-        'invoiceAddress' => $invoice_address,
-        'isActive' => $is_active
-    );
-
-    $customer = new Customers($array);
-    $customermanager = new CustomersManager($bdd);
-    $test = $customermanager->add($customer, $_POST["case"], $_POST["account"],$_POST["subaccount"], $_POST["taxes"]);
-
-    if($supplier == 1)
-    {
-      $supplier = new Suppliers($array);
-      $suppliermanager = new SuppliersManager($bdd);
-      $suppliermanager->add($supplier,$_POST["case"]);
-    }
-    
-    if(is_null($test)){
-        header('Location: '.URLHOST.$_COOKIE['company']."/client/afficher/error");
-    }else{
-        header('Location: '.URLHOST.$_COOKIE['company']."/client/afficher/success");
+        header('Location: http://test.bitwin.nc/index.php');
     }
 }
+else{
+    $customer2 = $customermanager->getByName($customer->getName());
+    $customer2 = $customermanager->getByID($customer2->getIdCustomer());
 
+    $test2 = $customermanager->duplicate($customer2, $_POST["case"][0]);
+}
 ?>
