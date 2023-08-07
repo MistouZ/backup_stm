59412b8ca41a6e8732f32c06b8a2850952036aae
Amaury DEWYNTER <40284577+MistouZ@users.noreply.github.com>
2023-08-07 16:26:39 +1100

    update script for customer

--- backup_customers.php
+++ backup_customers.php
@@ -17,10 +17,10 @@ else
 //récupération des données de l'utilisateur
 $bd = new PDO('mysql:host=localhost;port=3306; dbname=stm_test_db;charset=utf8', 'testuser', 'U!nx837j');
 
-$count = $bd->query('SELECT * FROM dossier  WHERE societe !="nmcp" AND societe !="hydro" AND `annee` >= 2022 GROUP BY client');
+$count = $bd->query('SELECT * FROM dossier  WHERE societe !="nmcp" AND societe !="hydro" AND `annee` >= 2022 AND client != "" GROUP BY client');
 $maxrow = $count->rowCount();
 
-$req = $bd->query('SELECT * FROM dossier  WHERE societe !="nmcp" AND societe !="hydro" AND `annee` >= 2022 GROUP BY client LIMIT '.$i.',1 ');
+$req = $bd->query('SELECT * FROM dossier  WHERE societe !="nmcp" AND societe !="hydro" AND `annee` >= 2022 AND client != "" GROUP BY client LIMIT '.$i.',1 ');
 $recup = $req->fetch();
 
 
